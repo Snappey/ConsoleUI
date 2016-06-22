5dd43049b9b691f20d6d5dd9214249794a4eebe4
Snappey <jonathan.nut@gmail.com>
2016-06-22 20:03:18 +0100

    Added Button

--- Elements/Button.cs
+++ Elements/Button.cs
@@ -2,13 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using ConsoleTUI.Drawing;
 
 namespace ConsoleTUI.Elements
 {
     public class Button : Panel
     {
-        public Button(int x, int y, int w, int h) : base(x, y, w, h)
+        public Button(int x, int y, int w, int h, Base parent=null) : base(x, y, w, h, parent)
         {
+            Paint += PaintPanel;
+        }
+
+        public override void PaintPanel(object obj, PaintEventArgs e)
+        {
+            Draw.SetBackground(ConsoleColor.Cyan);
+            Draw.Rect(X, Y, W, H);
+            Draw.ResetColours();
         }
     }
 }
