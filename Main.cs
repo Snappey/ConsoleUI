c1b459f9531528ee4399610a34aec6759715cfbb
Snappey <jonathan.nut@gmail.com>
2016-06-23 17:23:55 +0100

    Seperate Test Project

--- Main.cs
+++ Main.cs
@@ -3,9 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.IO;
-using ConsoleTUI;
-using ConsoleTUI.Drawing;
-using ConsoleTUI.Elements;
+using System.Threading;
+using ConsoleUI;
+using ConsoleUI.Drawing;
+using ConsoleUI.Elements;
 
 namespace ConsoleTUI
 {
@@ -13,14 +14,39 @@ namespace ConsoleTUI
     {
         static void Main(string[] args)
         {
-           
+         
+            Init();
+            StartThread();
+
             Panel pnl = new Panel(5,5,90,15);
     
             Label lbl = new Label(2,2,"Testing", pnl);
 
+            Button btn = new Button(2,6,20,5, pnl);
+
+            Footer ftr = new Footer(0);
+
+            for (int i = 0; i < Console.BufferHeight; i++)
+            {
+                Draw.Text(0, i, i.ToString(), ConsoleColor.White);
+            }
+
+          //  Console.CursorVisible = false;
 
             Console.ReadKey();
         }
-    }
 
+        private static void Init()
+        {      
+            Console.BufferWidth = 120;
+            Console.BufferHeight = 31;
+            Console.SetWindowSize(120, 30);
+        }
+
+        public static void StartThread()
+        {
+            Thread ConsoleEvents = new Thread(Hooks.CreateEvents);
+            ConsoleEvents.Start();
+        }
+    }
 }
