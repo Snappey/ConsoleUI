a08a9b902dd3918d405089d865b8663531d8670c
Snappey <jonathan.nut@gmail.com>
2016-06-25 20:33:30 +0100

    Updated Test Code

--- Main.cs
+++ Main.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.IO;
 using System.Threading;
-using ConsoleUI;
 using ConsoleUI.Drawing;
 using ConsoleUI.Elements;
+using ConsoleUI.Manager;
 
 namespace ConsoleTUI
 {
@@ -14,21 +14,23 @@ namespace ConsoleTUI
     {
         static void Main(string[] args)
         {
-         
+            var p = 0;
             Init();
-            StartThread();
+            Hooks.StartEvents();
+            Keys.StartKeys();
 
             Panel pnl = new Panel(5,5,90,15);
     
             Label lbl = new Label(2,2,"Testing", pnl);
+            lbl.SetTextColor(ConsoleColor.Black);
+
+            Keys.ConsoleKeyPressed += delegate(object sender, Keys.KeyEventArgs eventArgs) { p++; lbl.SetText(p.ToString()); };
 
             Button btn = new Button(2,6,20,5, pnl);
 
-            Footer ftr = new Footer(0);
-
             for (int i = 0; i < Console.BufferHeight; i++)
             {
-                Draw.Text(0, i, i.ToString(), ConsoleColor.White);
+                Label dbg = new Label(0, i, i.ToString());
             }
 
           //  Console.CursorVisible = false;
@@ -41,12 +43,6 @@ namespace ConsoleTUI
             Console.BufferWidth = 120;
             Console.BufferHeight = 31;
             Console.SetWindowSize(120, 30);
-        }
-
-        public static void StartThread()
-        {
-            Thread ConsoleEvents = new Thread(Hooks.CreateEvents);
-            ConsoleEvents.Start();
         }
     }
 }
