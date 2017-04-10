1cc5bcff6fa6f1cb352896f6e9ee559a62a11973
Snappey <jonathan.nut@gmail.com>
2017-04-10 19:47:49 +0100

    Cleanup / Restructure

---
+++ ConsoleUI/Interfaces/ISelectable.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI.Elements
+{
+    interface ISelectable
+    {
+        event EventHandler OnClick;
+        event EventHandler OnSelected;
+        event EventHandler OnSelectedChanged;
+
+        void DoClick();
+        void DoSelected();
+        void DoSelectedChanged();
+    }
+}
